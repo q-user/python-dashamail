61f9b28b249c645e008e155053141c0de727fcba
Mikhail Pavlov <mikhail.pavlov@internet.ru>
2023-04-14 13:38:27 +0300

    Correct campaigns_create optional arguments.

--- dashamail/client.py
+++ dashamail/client.py
@@ -307,7 +307,7 @@ class DashaMailClient:
     def lists_delete_merge(self, **params):
         raise NotImplementedError("TODO")
 
-    def campaigns_create(self, list_id: List[int], name: Optional[str], status: Optional[str],
+    def campaigns_create(self, list_id: List[int], name: Optional[str] = None, status: Optional[str] = 'DRAFT',
                          **params: Any) -> ResponseType:
         """
         Create a new campaign.
